88a7589c98c32bfd6fbe47e276d25ce0e2cc721d
Emil Åström <emil.astrom@knightec.se>
2022-12-05 15:45:35 +0100

    Fix

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,15 +1,36 @@
+use std::num::ParseIntError;
 use std::{collections::VecDeque, fs};
+use std::str::FromStr;
 
 fn read_file(path: &str) -> Vec<String> {
     let content = fs::read_to_string(path).expect("Error");
     return content.split('\n').map(|s| s.to_string()).collect();
 }
 
-struct Crate {
-    sign: String,
+struct Move {
+    amount: i32,
+    from: usize,
+    to: usize
 }
 
-fn setup_cargo(input: Vec<String>) -> (Vec<VecDeque<Crate>>, Vec<String>) {
+impl FromStr for Move {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let c: Vec<&str> = s.split(' ').collect::<Vec<&str>>();
+        let amount = c.get(1).unwrap().parse::<i32>()?;
+        let from = c.get(3).unwrap().parse::<usize>()? - 1;
+        let to = c.get(5).unwrap().parse::<usize>()? - 1;
+
+        Ok(Move {
+            amount,
+            from,
+            to,
+        })
+    }
+}
+
+fn setup_cargo(input: Vec<String>) -> (Vec<VecDeque<String>>, Vec<Move>) {
     let mut rows = input
         .iter()
         .map_while(|s| {
@@ -20,28 +41,26 @@ fn setup_cargo(input: Vec<String>) -> (Vec<VecDeque<Crate>>, Vec<String>) {
                 .chars()
                 .collect::<Vec<char>>()
                 .chunks(4)
-                .map(|c| c.get(1).unwrap().to_string())
-                .collect::<Vec<String>>();
+                .map(|c| {
+                    let s = c.get(1).unwrap().to_string();
+                    if !s.trim().is_empty() {Some(s)} else {None}
+                })
+                .collect::<Vec<Option<String>>>();
             Some(row)
         })
-        .collect::<Vec<Vec<String>>>();
+        .collect::<Vec<Vec<Option<String>>>>();
 
     let move_start = rows.len();
     rows.remove(rows.len() - 1);
 
     let cargo_width = rows.first().unwrap().len();
 
-    let mut cargo: Vec<VecDeque<Crate>> = Vec::new();
-    let mut i = 0;
-    while i < cargo_width {
-        cargo.push(VecDeque::new());
-        i += 1;
-    }
+    let mut cargo: Vec<VecDeque<String>> = vec![VecDeque::new(); cargo_width];
 
     rows.iter().for_each(|row| {
         row.iter().enumerate().for_each(|(column, c)| {
-            if !c.trim().is_empty() {
-                cargo[column].push_front(Crate { sign: c.to_owned() })
+            if c.is_some() {
+                cargo[column].push_front(c.to_owned().unwrap())
             }
         });
     });
@@ -49,71 +68,64 @@ fn setup_cargo(input: Vec<String>) -> (Vec<VecDeque<Crate>>, Vec<String>) {
     let moves = input
         .to_owned()
         .drain(move_start + 1..input.len())
-        .collect::<Vec<String>>();
+        .map(|s| s.parse::<Move>().unwrap())
+        .collect::<Vec<Move>>();
 
     (cargo, moves)
 }
 
 fn part1(input: Vec<String>) {
     let (mut cargo, moves) = setup_cargo(input);;
-    moves.iter().for_each(|s| {
-        let c: Vec<String> = s.split(' ').map(|s| s.to_string()).collect();
-        let amount = c.get(1).unwrap().parse::<i32>().unwrap();
-        let from = c.get(3).unwrap().parse::<usize>().unwrap() - 1;
-        let to = c.get(5).unwrap().parse::<usize>().unwrap() - 1;
-
+    moves.iter().for_each(|m| {
         let mut j = 0;
 
-        while j < amount {
-            let c = cargo.get_mut(from).unwrap().pop_back().unwrap();
-            cargo.get_mut(to).unwrap().push_back(c);
+        while j < m.amount {
+            let c = cargo.get_mut(m.from).unwrap().pop_back().unwrap();
+            cargo.get_mut(m.to).unwrap().push_back(c);
 
             j += 1;
         }
     });
 
     for ele in cargo {
-        print!("{:}", ele.back().unwrap().sign);
+        print!("{:}", ele.back().unwrap());
     }
 }
 
 fn part2(input: Vec<String>) {
     let (mut cargo, moves) = setup_cargo(input);
 
-    moves.iter().for_each(|s| {
-        let c: Vec<String> = s.split(' ').map(|s| s.to_string()).collect();
-        let amount = c.get(1).unwrap().parse::<i32>().unwrap();
-        let from = c.get(3).unwrap().parse::<usize>().unwrap() - 1;
-        let to = c.get(5).unwrap().parse::<usize>().unwrap() - 1;
+    moves.iter().for_each(|m| {
 
         let mut j = 0;
-        let mut stage: VecDeque<Crate> = VecDeque::new();
+        let mut stage: VecDeque<String> = VecDeque::new();
 
-        while j < amount {
-            let c = cargo.get_mut(from).unwrap().pop_back().unwrap();
+        while j < m.amount {
+            let c = cargo.get_mut(m.from).unwrap().pop_back().unwrap();
             stage.push_back(c);
 
             j += 1;
         }
 
         j = 0;
-        while j < amount {
+
+        while j < m.amount {
             let c = stage.pop_back().unwrap();
-            cargo.get_mut(to).unwrap().push_back(c);
+            cargo.get_mut(m.to).unwrap().push_back(c);
             j += 1;
         }
     });
 
     for ele in cargo {
-        print!("{:}", ele.back().unwrap().sign);
+        print!("{:}", ele.back().unwrap());
     }
 }
 
 fn main() {
     // let input = read_file("./src/test_input.txt");
     let input = read_file("./src/part1_input.txt");
-    part1(input.to_owned());
+    part1(input.to_owned()); // NTWZZWHFV
     println!();
-    part2(input);
+    part2(input); // BRZGFVBTJ
     println!();
 }
